3d1b9f92d12f709cfbea5b4ac527c1f541ec098f
Shivam Maurya <sm5708251@gmail.com>
2025-01-29 20:03:47 +0530

    Some Changes In Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "./Components/Navbar/Navbar"
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import Services from './Components/Services/Servisecs'
+// import Services from './Components/Services/Servisecs'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
@@ -12,7 +12,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
-      <Services/>
+      {/* <Services/> */}
       <MyWork/>
       <Contact/>
       <Footer/>
