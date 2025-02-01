aa773d3aa97595130c694130e5460f224c4e232d
Shivam Maurya <sm5708251@gmail.com>
2025-02-01 22:24:20 +0530

    Change code

--- src/Components/OpenChat/OpenChat.jsx
+++ src/Components/OpenChat/OpenChat.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./OpenChat.css"
 import chat from "../../assets/chat.png"
+import {motion} from "motion/react"
 
 const OpenChat = () => {
   return (
-    <div className='whatsapp'>
+    <motion.div 
+    className='whatsapp'
+    
+    >
+        
       <a href="https://wa.me/919792721939" target='_blanck'><img src={chat} alt="" className="chat-icon" /></a>
-    </div>
+    </motion.div>
   )
 }
 
